0c79551cbe23b7540b7851df8c3875a909003d58
Isaiah Norton <isaiah.norton@gmail.com>
2021-08-07 08:16:37 -0400

    implement C# wrapper

---
+++ 2021-csharp-call-cpp-array-lib/ProcessArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ProcessArray
+{
+    public class AuxWrapper
+    {
+        [DllImport("../cpp/build/libaux.dylib")]
+        public static extern int aux_sum_double(
+           IntPtr data, IntPtr nelem, double[] outptr
+        );
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var data = new double[4]{1,2,3,4};
+            var outptr = new double[1]{0};
+            var size_p = new IntPtr(data.GetLength(0));
+
+            // works w/
+            //   double[] data, IntPtr nelem, double[] outptr
+            //AuxWrapper.aux_sum_double(data, size_p, outptr);
+
+            // works w/ this
+            //   IntPtr data, IntPtr nelem, double[] outptr
+            GCHandle h_data = GCHandle.Alloc(data, GCHandleType.Pinned);
+            AuxWrapper.aux_sum_double(h_data.AddrOfPinnedObject(), size_p, outptr);
+
+            Console.WriteLine("result: " + outptr[0]);
+        }
+    }
+}

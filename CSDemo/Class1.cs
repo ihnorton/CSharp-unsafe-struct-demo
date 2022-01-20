5e7b1a81dc96faee36dd5ef5e5856de064343244
Isaiah Norton <isaiah.norton@gmail.com>
2022-01-20 10:46:39 -0500

    Update

--- CSDemo/Class1.cs
+++ CSDemo/Class1.cs
@@ -2,18 +2,28 @@
 using System.Runtime.InteropServices;
 public static class LibFoo {
 
+  public unsafe struct handle_t {};
+
   [DllImport("libfoo.dylib")]
-  internal unsafe static extern void
+  public unsafe static extern void
   doit(int valin, int *res);
+
+  [DllImport("libfoo.dylib")]
+  public unsafe static extern void handle_alloc(handle_t **h);
 }
 
 namespace CSDemo {
-public class Class1 {
+public unsafe class Class1 {
   public unsafe static void Main() {
     int res = 0;
     LibFoo.doit(2, &res);
 
     Console.WriteLine("result: {0}", res);
+
+    LibFoo.handle_t* h;
+    LibFoo.handle_alloc(&h);
+
+    Console.WriteLine("C# handle_t* is: {0:X}", (UInt64)h);
   }
 }
 }
--- CSDemo/foo.c
+++ CSDemo/foo.c
@@ -1,7 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct handle_t {
   int internal;
 } handle_t;
 
-void handle_alloc(handle_t **p) { *p = malloc(sizeof(handle_t)); }
+void handle_alloc(handle_t **p) {
+  *p = malloc(sizeof(handle_t));
+
+  printf("handle_t* is: %p\n", *p);
+}
 
 void doit(int in, int *out) { *out = in + 10; }
